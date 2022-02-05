ad2130c5b4530b6119c5d2546702fed4574a0f50
Mishal Rai <minkumarrai@gmail.com>
2022-02-05 22:39:35 +0545

    pages routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () =>  import('./login/login.module').then( m => m.LoginModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'setting',
+    loadChildren: () => import('./setting/setting.module').then(m => m.SettingModule)
+  },
+  {
+    path: 'add-product',
+    loadChildren: () => import('./add-product/add-product.module').then(m => m.AddProductModule)
+  },
+  {
+    path: 'add-product/:id',
+    loadChildren: () => import('./detail-page/detail-page.module').then(m => m.DetailPageModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
